a919c61e4c9f1aab4f081517d84c82601c03c10a
minjilee <budlalswl1129@gmail.com>
2024-05-05 00:29:08 +0900

    feat: 관리자 화면 outlet 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { CartProvider } from './contexts/cart-context';
 import MenuList from './pages/MenuList/index';
+import AdminOutlet from './pages/AdminOutlet';
+import AdminMenu from './pages/AdminMenu/index';
 
 function App() {
     return (
         <CartProvider>
-            <MenuList />
+            <Routes>
+                <Route path='/menus' element={<MenuList />} />
+                <Route path='/admin' element={<AdminOutlet />}>
+                    <Route path=':menu' element={<AdminMenu />} />
+                </Route>
+            </Routes>
         </CartProvider>
     );
 }
---
+++ src/pages/AdminMenu/index.tsx
@@ -0,0 +1,4 @@
+function AdminMenu() {
+    return <></>;
+}
+export default AdminMenu;
