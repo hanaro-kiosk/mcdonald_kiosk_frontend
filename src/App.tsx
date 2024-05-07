e34ac21bb78b8941afa946cc1c7c2e2f459aa80a
안나영 <dpswpfskdud@naver.com>
2024-05-07 10:35:54 +0900

    feat: 결제화면 & 유저 포인트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
+import OrderPayment from './pages/OrderPayment';
+import { OrderWithUserPoint } from './pages/OrderWithUserPoint';
 import Home from './pages/home';
 
 function App() {
     return (
         <>
-            <Home />
+            <OrderWithUserPoint orderNumber={200000} userPoint={10} />
         </>
     );
 }
---
+++ src/pages/OrderPayment/index.tsx
@@ -0,0 +1,41 @@
+import { FC, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export interface OrderPaymentProps {
+    totalPrice: number;
+    totalCount: number;
+}
+
+const OrderPayment: FC<OrderPaymentProps> = ({ totalPrice, totalCount }) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            navigate('/OrderWithUserPoint');
+        }, 3000);
+
+        return () => clearTimeout(timeout);
+    }, [navigate]);
+
+    return (
+        <div>
+            <h1 className='pt-5 pb-5 text-center text-white'>
+                주문을 확인하세요.
+            </h1>
+
+            <div className='p-2 mx-10 mt-2 text-center bg-white border rounded-lg'>
+                <span>총 수량 : {totalCount}개</span>
+                <span className='text-red-600'> 총 가격 : {totalPrice}원</span>
+            </div>
+            <div className='p-2 mx-10 mt-20 text-center bg-white border rounded-lg'>
+                <p>
+                    카드를 화살표 방향으로 투입구에 넣어주세요. 결제 오류시,
+                    카드를 긁어주세요.
+                </p>
+                <img src='./public/assets/payment.png' className='p-4' />
+            </div>
+        </div>
+    );
+};
+
+export default OrderPayment;
