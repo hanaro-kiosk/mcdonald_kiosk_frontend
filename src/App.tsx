6263b1d228ab167a0ca4c7e5326ca14b871c4af1
tlsaudwl <tlsaudwl1821@naver.com>
2024-05-07 22:23:22 +0900

    design: css파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Join from './pages/User/Join/index';
 import Login from './pages/User/Login/index';
-import Admin from './pages/Admin/User/index';
 
 function App() {
     return (
@@ -10,7 +9,7 @@ function App() {
             <Routes>
                 <Route path='/join' element={<Join />} />
                 <Route path='/login' element={<Login />} />
-                <Route path='/admin' element={<Admin />} />
+                {/* <Route path='/admin' element={<Admin />} /> */}
             </Routes>
         </Router>
     );
